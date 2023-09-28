e13e03ea8bf528a9b88226f7d399019286711d05
theogguu <38506698+theogguu@users.noreply.github.com>
2023-09-28 02:29:23 -0500

    show courses

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 
 const schedule = {
-  title: "CS Courses for 2018-2019"
+  "title": "CS Courses for 2018-2019",
+  "courses": {
+    "F101" : {
+      "term": "Fall",
+      "number": "101",
+      "meets" : "MWF 11:00-11:50",
+      "title" : "Computer Science: Concepts, Philosophy, and Connections"
+    },
+    "F110" : {
+      "term": "Fall",
+      "number": "110",
+      "meets" : "MWF 10:00-10:50",
+      "title" : "Intro Programming for non-majors"
+    },
+    "S313" : {
+      "term": "Spring",
+      "number": "313",
+      "meets" : "TuTh 15:30-16:50",
+      "title" : "Tangible Interaction Design and Learning"
+    },
+    "S314" : {
+      "term": "Spring",
+      "number": "314",
+      "meets" : "TuTh 9:30-10:50",
+      "title" : "Tech & Human Interaction"
+    }
+  }
 };
+
 
 const App = () => {
  return (
