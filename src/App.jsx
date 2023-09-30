739a1978475d6a9728b862b0359d587a8350ab10
theogguu <38506698+theogguu@users.noreply.github.com>
2023-09-30 16:02:11 -0500

    fetch complete

--- src/App.jsx
+++ src/App.jsx
@@ -8,45 +8,33 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // npm i bootstrap-icons
 // import 'bootstrap-icons/font/bootstrap-icons.css';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useJsonQuery } from './utilities/fetch';
 
-const schedule = {
-  "title": "CS Courses for 2018-2019",
-  "courses": {
-    "F101" : {
-      "term": "Fall",
-      "number": "101",
-      "meets" : "MWF 11:00-11:50",
-      "title" : "Computer Science: Concepts, Philosophy, and Connections"
-    },
-    "F110" : {
-      "term": "Fall",
-      "number": "110",
-      "meets" : "MWF 10:00-10:50",
-      "title" : "Intro Programming for non-majors"
-    },
-    "S313" : {
-      "term": "Spring",
-      "number": "313",
-      "meets" : "TuTh 15:30-16:50",
-      "title" : "Tangible Interaction Design and Learning"
-    },
-    "S314" : {
-      "term": "Spring",
-      "number": "314",
-      "meets" : "TuTh 9:30-10:50",
-      "title" : "Tech & Human Interaction"
-    }
-  }
-};
+const Main = () => {
+  const [data, isLoading, error] = useJsonQuery('https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php');
 
+  if (error) return <h1>Error loading user data: {`${error}`}</h1>;
+  if (isLoading) return <h1>Loading user data...</h1>;
+  if (!data) return <h1>No user data found</h1>;
 
-const App = () => {
- return (
-    <div className="container">
-      <Banner title={schedule.title} />
-      <CourseList courses={schedule.courses} />
+  return (
+    <div className="main">
+      <Banner title={data.title} />
+      <CourseList courses={data.courses} />
     </div>
- )
+  )
 }
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>  
+    <div className="container">
+      <Main />
+    </div>
+  </QueryClientProvider>
+ )
+
   
-  export default App;
+export default App;
